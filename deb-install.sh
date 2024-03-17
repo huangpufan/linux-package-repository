ce41162d782e4bc714c4d78e2d0d12a2928148be
huangpufan <59730801@qq.com>
2024-03-17 14:56:23 +0800

    Add duf bat

---
+++ deb-install.sh
@@ -0,0 +1,14 @@
+# 找到当前目录下所有的 .deb 文件并通过 dpkg 安装
+for deb in ./*.deb; do
+    if [[ -f "$deb" ]]; then
+        echo "Installing $deb"
+        sudo dpkg -i "$deb"
+        # 如果 dpkg 有依赖问题，尝试解决它们
+        sudo apt-get -f install
+    else
+        echo "No .deb files found."
+        exit 1
+    fi
+done
+
+echo "All .deb files have been installed."
